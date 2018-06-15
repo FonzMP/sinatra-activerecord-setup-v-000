608904316146218ee7749d011700c768109de78b
Alex Wilson <fonzmp@gmail.com>
2018-06-15 15:50:54 +0000

    automatically backed up by learn

--- db/migrate/20180615154940_create_dogs.rb
+++ db/migrate/20180615154940_create_dogs.rb
@@ -1,11 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
   def up
-  end
-
-  def down
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
+  end
+
+  def down
+  
   end
 end
